126a3312d40097990157429644030871bbfc7127
Daniel Serrano <ingm.daniel.serrano@gmail.com>
2021-07-27 15:17:48 -0500

    arreglando  header

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+//import Image from 'next/image'
+// next
+import Link from 'next/link';
+//material ui 
+import { Grid } from '@material-ui/core';
+// styles
+import styles from '../../styles/layout.module.scss';
 
 const Header = () => {
   return (  
-    <div><h1>Header</h1></div>
+    <header className = { styles.header}>
+      <Grid container>
+        <Grid item xs= {6}> 
+          <Link href = '/'>
+            <h1>Got It</h1>
+          </Link>
+        </Grid>
+        <Grid item xs= {6}> </Grid>
+      </Grid>
+    </header>
   );
 }
  
