c7c6da3fe6c2e83b11a5ac2443e69d9c9f7954f3
Daniel Serrano <ingm.daniel.serrano@gmail.com>
2021-07-27 13:44:58 -0500

    creando paginas

---
+++ pages/projects/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/layout'
+
+const Proyects = () => {
+  return(
+    <Layout>
+      <div><h2>Proyects</h2></div>
+    </Layout>
+  );
+}
+ 
+export default Proyects;
+
