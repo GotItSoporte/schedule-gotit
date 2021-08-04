e61be93a8b329715ee5a6c09c6464ad1dae93ef3
Daniel Serrano <ingm.daniel.serrano@gmail.com>
2021-08-04 16:56:40 -0500

    agregando mensajes de errores

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,43 +1,43 @@
 import React, { useEffect } from 'react';
 // routing
 import { useRouter } from 'next/router';
+// context 
+import useUser from '../context/hooks/useUser';
 // componets
-import Header from '../components/layout/Header';
+import ErrorForm from '../components/ErrorForm';
 //styles
 import styles from '../styles/forms.module.scss'
 // form validation
 import { Formik, Form, Field } from 'formik';
- import * as Yup from 'yup';
-// context 
-import useUser from '../context/hooks/useUser';
+import * as Yup from 'yup';
 
  const loginSchema = Yup.object().shape({
    userName: Yup.string()
-     .required('Required'),
-   password: Yup.string()
-     .min(5, 'La contraseña debe ser de 6 o mas caracteres!'),
+     .required('El nombre de ususario es obligatorio'),
+   password: Yup.string().required('El password es obligarotio')
  });
 
 const Login = () => {
   // User context
   const userContext = useUser();
   const { userLogin, state : userState, isAuthenticated } = userContext; 
-  const { isAuth, user } = userState;
+  const { isAuth, user, message } = userState;
+
   // next Router
   const router = useRouter();
-  let  onsubmit = false;
+
   useEffect(() => {
-    console.log( 'refresh', {userState, isAuth});
-    isAuthenticated();
+    console.log( 'refresh', { isAuth});
     if( isAuth ){
       console.log( 'auth',{ userState, isAuth })
       router.push({ pathname : '/', query : { company : user?.company } });
     }else{
+      isAuthenticated();
       console.log( 'auth', false )
     }
   }, [ isAuth ])
   return ( 
-    <>
+
         <div className={ styles.login }>
           <h1>Login</h1>
           <Formik
@@ -46,11 +46,10 @@ const Login = () => {
             password: '',
            }}
            validationSchema={loginSchema}
-           onSubmit={values => {
+           onSubmit={ async values => {
              // same shape as initial values
              console.log('SUBMIT')
-             userLogin(values);
-             onsubmit = true;
+             await userLogin(values);
            }}
           >
             {({ errors, touched }) => (
@@ -63,7 +62,10 @@ const Login = () => {
                   required="required"  
                 />
                 {errors.userName && touched.userName ? (
-                <div>{errors.userName}</div>
+                <ErrorForm 
+                  classStyle= { styles.error_message } 
+                  errorMessage = {errors.userName}
+                />
                 ) : null}
 
                 <Field 
@@ -74,15 +76,17 @@ const Login = () => {
                   required="required" 
                 />
                 {errors.password && touched.password ? (
-                <div>{errors.password}</div>
+                <ErrorForm 
+                  classStyle= { styles.error_message } 
+                  errorMessage = {errors.password}
+                />
                 ) : null}
 
                 <button type="submit" className={ styles.btn }>Iniciar Sesión</button>
               </Form>
             )}
-     </Formik>
+          </Formik>
         </div>
-    </>
   );
 }
  
