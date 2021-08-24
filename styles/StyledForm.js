74bdd78ac7fb6d12f6386667f854f921ac921131
Daniel Serrano <ingm.daniel.serrano@gmail.com>
2021-08-24 17:31:43 -0500

    Create StyledForm.js

---
+++ styles/StyledForm.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledForm =styled.form`
+  box-shadow: rgb(0 0 0 / 10%) 0px 10px 15px -3px, 
+		rgb(0 0 0 / 5%) 0px 4px 6px -2px;
+	margin: 2rem 0rem 2rem 0rem;
+	padding: 1rem 8rem 1rem 8rem;
+  
+`;
