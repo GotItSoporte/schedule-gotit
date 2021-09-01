e65f41f860534a3ba3fad09ee034d66bbbb4b204
Daniel Serrano <ingm.daniel.serrano@gmail.com>
2021-09-01 14:27:13 -0500

    probar taskform

--- components/forms/TaskForm.jsx
+++ components/forms/TaskForm.jsx
@@ -75,54 +75,7 @@ const TaskForm = ({  submitFunction, edit, initialValues }) => {
         >
           <h1>EDITAR TAREA</h1>
           <form onSubmit={formik.handleSubmit} >
-            <TextInput 
-              type="text"
-              placeholder="Título" 
-              name="time" 
-  
-              value = { formik.values.time }
-              onChange = { formik.handleChange }
-              error = { formik.touched.time && Boolean( formik.errors.time ) }
-              helperText={formik.touched.time && formik.errors.time}
-              />
-            <SessionsInput  
-              sessions = { formik.values.sessions } 
-              formikSetFieldValue = {  formik.setFieldValue  }
-              
-              formikTouchedSessions = { formik.touched.sessions }
-              formikErrorsSessions = { formik.errors.sessions }
-            />
-            <Grid
-              container
-              direction = 'column'
-            >
-              <CheckBox
-                id = 'finished'
-                checked = { formik.values.finished }
-                onBlur = { formik.handleBlur }
-                onChange = { formik.handleChange }
-                
-                label="Dar el requerimiento por Cerrado"
-              />
-              <CheckBox
-                id = 'success'
-                checked = { formik.values.success }
-                onBlur = { formik.handleBlur }
-                onChange = { formik.handleChange }
-
-                label="Se Ha dado solución al requerimiento"
-              />
-            </Grid>
-            <TextAreaIput 
-              name="description" 
-              placeholder="Por favor escriba suna breve descripción de la solución" 
-  
-              value = { formik.values.description }
-              onChange = { formik.handleChange }
-              error = { formik.touched.description && Boolean( formik.errors.description ) }
-              helperText={formik.touched.description && formik.errors.description}
-  
-            />
+           
             <SubmitButton textButton = { 'ENviar'} />
           </form>
 
